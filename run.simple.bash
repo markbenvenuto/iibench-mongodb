6e63d0a3ff66cd7b6467845330d7f818e5b15307
Mark Benvenuto <mark.benvenuto@mongodb.com>
2015-01-23 11:34:39 -0500

    mc repro

--- run.simple.bash
+++ run.simple.bash
@@ -1,22 +1,23 @@
-#!/bin/bash
-
+#!/bin/bash 
 # simple script to run against running MongoDB/TokuMX server localhost:(default port)
 
 # if running TokuMX, need to select compression for collection and secondary indexes (zlib is default)
 #   valid values : lzma, quicklz, zlib, none
-export MONGO_COMPRESSION=zlib
+export MONGO_COMPRESSION=quicklz
 
 # if running TokuMX, need to select basement node size (65536 is default)
 #   valid values : integer > 0 : 65536 for 64K
-export MONGO_BASEMENT=65536
+# export MONGO_BASEMENT=65536
+export MONGO_BASEMENT=16384
 
 # run the benchmark for this many inserts (or the number of minutes defined by RUN_MINUTES)
 #   valid values : integer > 0
-export MAX_ROWS=100000000
+export MAX_ROWS=200000000
+#export MAX_ROWS=10000000
 
 # run the benchmark for this many minutes (or the number of inserts defined by MAX_ROWS)
 #   valid values : intever > 0
-export RUN_MINUTES=60
+export RUN_MINUTES=200000
 export RUN_SECONDS=$[RUN_MINUTES*60]
 
 # total number of documents to insert per "batch"
@@ -71,38 +72,38 @@ export NUM_SECONDARY_INDEXES=3
 
 # the following 4 parameters allow an insert plus query workload benchmark
 
-# number of queries to perform per QUERY_INTERVAL_SECONDS seconds
-#   valid values : integer > 0, set to zero for insert only workload
-export QUERIES_PER_INTERVAL=0
-
-# number of seconds during which to perform QUERIES_PER_INTERVAL queries
-#   valid values : integer > 0
-export QUERY_INTERVAL_SECONDS=15
-
 # number of documents to return per query
 #   valid values : integer > 0
-export QUERY_LIMIT=10
+export QUERY_LIMIT=4
 
 # wait this many inserts to begin the query workload
 #   valid values : integer > 0
-export QUERY_NUM_DOCS_BEGIN=1000000
+export QUERY_NUM_DOCS_BEGIN=100000
+
+# number of query threads, must be >= 0
+export QUERY_THREADS=1
+
+# sleep time before a thread starts the next query, must be >= 0
+export MS_BETWEEN_QUERIES=0
 
 # create the collection
 #   valid values : Y/N
 export CREATE_COLLECTION=Y
 
-
+JAVA=java
+CLASSPATH=$PWD/mongo-java-driver-2.11.4.jar
 javac -cp $CLASSPATH:$PWD/src src/jmongoiibench.java
 
 
-export LOG_NAME=mongoiibench-${MAX_ROWS}-${NUM_DOCUMENTS_PER_INSERT}-${MAX_INSERTS_PER_SECOND}-${NUM_LOADER_THREADS}-${QUERIES_PER_INTERVAL}-${QUERY_INTERVAL_SECONDS}.txt
+export LOG_NAME=mongoiibench-${MAX_ROWS}-${NUM_DOCUMENTS_PER_INSERT}-${MAX_INSERTS_PER_SECOND}-${NUM_LOADER_THREADS}-${QUERY_THREADS}-${MS_BETWEEN_QUERIES}.txt
 export BENCHMARK_TSV=${LOG_NAME}.tsv
     
 rm -f $LOG_NAME
 rm -f $BENCHMARK_TSV
 
 T="$(date +%s)"
-java -cp $CLASSPATH:$PWD/src jmongoiibench $DB_NAME $NUM_LOADER_THREADS $MAX_ROWS $NUM_DOCUMENTS_PER_INSERT $NUM_INSERTS_PER_FEEDBACK $NUM_SECONDS_PER_FEEDBACK $BENCHMARK_TSV $MONGO_COMPRESSION $MONGO_BASEMENT $RUN_SECONDS $QUERIES_PER_INTERVAL $QUERY_INTERVAL_SECONDS $QUERY_LIMIT $QUERY_NUM_DOCS_BEGIN $MAX_INSERTS_PER_SECOND $WRITE_CONCERN $MONGO_SERVER $MONGO_PORT $NUM_CHAR_FIELDS $LENGTH_CHAR_FIELDS $NUM_SECONDARY_INDEXES $PERCENT_COMPRESSIBLE $CREATE_COLLECTION | tee -a $LOG_NAME
+$JAVA -cp $CLASSPATH:$PWD/src jmongoiibench $DB_NAME $NUM_LOADER_THREADS $MAX_ROWS $NUM_DOCUMENTS_PER_INSERT $NUM_INSERTS_PER_FEEDBACK $NUM_SECONDS_PER_FEEDBACK $BENCHMARK_TSV $MONGO_COMPRESSION $MONGO_BASEMENT $RUN_SECONDS $QUERY_LIMIT $QUERY_NUM_DOCS_BEGIN $MAX_INSERTS_PER_SECOND $WRITE_CONCERN $MONGO_SERVER $MONGO_PORT $NUM_CHAR_FIELDS $LENGTH_CHAR_FIELDS $NUM_SECONDARY_INDEXES $PERCENT_COMPRESSIBLE $CREATE_COLLECTION $QUERY_THREADS $MS_BETWEEN_QUERIES 2>&1 | tee -a $LOG_NAME
 echo "" | tee -a $LOG_NAME
 T="$(($(date +%s)-T))"
 printf "`date` | iibench duration = %02d:%02d:%02d:%02d\n" "$((T/86400))" "$((T/3600%24))" "$((T/60%60))" "$((T%60))" | tee -a $LOG_NAME
+
