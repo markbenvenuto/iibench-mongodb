9ea6838db37e573c85f44d3eb4e973758e8d3f79
Mark Benvenuto <mark.benvenuto@mongodb.com>
2015-01-28 11:51:26 -0500

    mc4

--- run.simple.bash
+++ run.simple.bash
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/bin/bash 
 # simple script to run against running MongoDB/TokuMX server localhost:(default port)
 
 # if running TokuMX, need to select compression for collection and secondary indexes (zlib is default)
@@ -8,20 +7,24 @@ export MONGO_COMPRESSION=zlib
 
 # if running TokuMX, need to select basement node size (65536 is default)
 #   valid values : integer > 0 : 65536 for 64K
-export MONGO_BASEMENT=65536
+# export MONGO_BASEMENT=65536
+export MONGO_BASEMENT=16384
 
 # run the benchmark for this many inserts (or the number of minutes defined by RUN_MINUTES)
 #   valid values : integer > 0
+# export MAX_ROWS=200000000
+# export MAX_ROWS=400000000
 export MAX_ROWS=100000000
+export MAX_ROWS=400000000
 
 # run the benchmark for this many minutes (or the number of inserts defined by MAX_ROWS)
 #   valid values : intever > 0
-export RUN_MINUTES=60
+export RUN_MINUTES=$1
 export RUN_SECONDS=$[RUN_MINUTES*60]
 
 # total number of documents to insert per "batch"
 #   valid values : integer > 0
-export NUM_DOCUMENTS_PER_INSERT=1000
+export NUM_DOCUMENTS_PER_INSERT=10
 
 # total number of documents to insert per second, allows for the benchmark to be rate limited
 #   valid values : integer > 0
@@ -29,7 +32,7 @@ export MAX_INSERTS_PER_SECOND=999999
 
 # total number of simultaneous insertion threads
 #   valid values : integer > 0
-export NUM_LOADER_THREADS=1
+export NUM_LOADER_THREADS=10
 
 # database in which to run the benchmark
 #   valid values : character
@@ -59,11 +62,11 @@ export NUM_CHAR_FIELDS=1
 
 # size (in bytes) of each additional semi-compressible character field
 #   valid values : integer >= 0
-export LENGTH_CHAR_FIELDS=1000
+export LENGTH_CHAR_FIELDS=2000
 
 # percentage of highly compressible data (repeated character "a") in character field
 #   valid values : integer >= 0 and <= 100
-export PERCENT_COMPRESSIBLE=90
+export PERCENT_COMPRESSIBLE=50
 
 # number of secondary indexes to maintain
 #   valid values : integer >= 0 and <= 3
@@ -71,38 +74,44 @@ export NUM_SECONDARY_INDEXES=3
 
 # the following 4 parameters allow an insert plus query workload benchmark
 
-# number of queries to perform per QUERY_INTERVAL_SECONDS seconds
-#   valid values : integer > 0, set to zero for insert only workload
-export QUERIES_PER_INTERVAL=0
-
-# number of seconds during which to perform QUERIES_PER_INTERVAL queries
-#   valid values : integer > 0
-export QUERY_INTERVAL_SECONDS=15
-
 # number of documents to return per query
 #   valid values : integer > 0
-export QUERY_LIMIT=10
+export QUERY_LIMIT=4
 
 # wait this many inserts to begin the query workload
 #   valid values : integer > 0
-export QUERY_NUM_DOCS_BEGIN=1000000
+export QUERY_NUM_DOCS_BEGIN=100000
+
+# number of query threads, must be >= 0
+export QUERY_THREADS=0
+
+# sleep time before a thread starts the next query, must be >= 0
+export MS_BETWEEN_QUERIES=0
 
 # create the collection
 #   valid values : Y/N
 export CREATE_COLLECTION=Y
 
+JAVA=java
+CLASSPATH=mongo-java-driver-2.11.4.jar
+ javac -cp $CLASSPATH:$PWD/src src/jmongoiibench.java
 
-javac -cp $CLASSPATH:$PWD/src src/jmongoiibench.java
 
-
-export LOG_NAME=mongoiibench-${MAX_ROWS}-${NUM_DOCUMENTS_PER_INSERT}-${MAX_INSERTS_PER_SECOND}-${NUM_LOADER_THREADS}-${QUERIES_PER_INTERVAL}-${QUERY_INTERVAL_SECONDS}.txt
+export LOG_NAME=mongoiibench-${MAX_ROWS}-${NUM_DOCUMENTS_PER_INSERT}-${MAX_INSERTS_PER_SECOND}-${NUM_LOADER_THREADS}-${QUERY_THREADS}-${MS_BETWEEN_QUERIES}.txt
 export BENCHMARK_TSV=${LOG_NAME}.tsv
     
 rm -f $LOG_NAME
 rm -f $BENCHMARK_TSV
 
+python mstat.py --loops 1000000 --interval 10 >& o.mstat.r &
+mid=$!
+
 T="$(date +%s)"
-java -cp $CLASSPATH:$PWD/src jmongoiibench $DB_NAME $NUM_LOADER_THREADS $MAX_ROWS $NUM_DOCUMENTS_PER_INSERT $NUM_INSERTS_PER_FEEDBACK $NUM_SECONDS_PER_FEEDBACK $BENCHMARK_TSV $MONGO_COMPRESSION $MONGO_BASEMENT $RUN_SECONDS $QUERIES_PER_INTERVAL $QUERY_INTERVAL_SECONDS $QUERY_LIMIT $QUERY_NUM_DOCS_BEGIN $MAX_INSERTS_PER_SECOND $WRITE_CONCERN $MONGO_SERVER $MONGO_PORT $NUM_CHAR_FIELDS $LENGTH_CHAR_FIELDS $NUM_SECONDARY_INDEXES $PERCENT_COMPRESSIBLE $CREATE_COLLECTION | tee -a $LOG_NAME
+$JAVA -cp $CLASSPATH:$PWD/src jmongoiibench $DB_NAME $NUM_LOADER_THREADS $MAX_ROWS $NUM_DOCUMENTS_PER_INSERT $NUM_INSERTS_PER_FEEDBACK $NUM_SECONDS_PER_FEEDBACK $BENCHMARK_TSV $MONGO_COMPRESSION $MONGO_BASEMENT $RUN_SECONDS $QUERY_LIMIT $QUERY_NUM_DOCS_BEGIN $MAX_INSERTS_PER_SECOND $WRITE_CONCERN $MONGO_SERVER $MONGO_PORT $NUM_CHAR_FIELDS $LENGTH_CHAR_FIELDS $NUM_SECONDARY_INDEXES $PERCENT_COMPRESSIBLE $CREATE_COLLECTION $QUERY_THREADS $MS_BETWEEN_QUERIES 2>&1 | tee -a $LOG_NAME
 echo "" | tee -a $LOG_NAME
 T="$(($(date +%s)-T))"
 printf "`date` | iibench duration = %02d:%02d:%02d:%02d\n" "$((T/86400))" "$((T/3600%24))" "$((T/60%60))" "$((T%60))" | tee -a $LOG_NAME
+
+
+kill $mid
+
